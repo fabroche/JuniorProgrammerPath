b1ec67d5ed4fd5e4b4e42f25a0fd166c912673de
fabroche <kraxusmmo@gmail.com>
2026-08-16 20:06:39 +0100

    feat: camera offset added

--- Assets/Scripts/Camera/FollowPlayer.cs
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -5,16 +5,24 @@ using System.Collections.Generic;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    
+    [SerializeField] private Vector3 cameraOffset = new Vector3(0, 5, -7);
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + cameraOffset;
+    }
+
+    void FixedUpdate()
+    {
     }
 }
